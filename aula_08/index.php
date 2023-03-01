057f6482f7a7ebd0932664ddd57bb5a23775f6df
melon musk <125475444+JoaomBRosio@users.noreply.github.com>
2023-03-01 15:38:22 -0300

    Add files via upload

---
+++ aula_08/index.php
@@ -0,0 +1,26 @@
+<?php
+
+//opeeracoes matematicas
+
+$num = 7+12;
+
+$num2 = 53-1;
+
+$resultado = $num + $num2;
+$resultado2 = 10/5 ;
+
+//echo $resultado2;
+
+
+//Contas
+//$conta = 10 + (50 * 2);
+//echo $conta;
+
+
+$resto = 1 % 2;
+echo $resto;
+
+//SE O RESTO É IGUAL A 0
+//SABEMOS QUE TEMOS O NUMERO PAR
+
+?> 
